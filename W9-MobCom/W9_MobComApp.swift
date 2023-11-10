7ab9de29f532062b0fa8b0ff1b24ff3d62bd0716
GamalielFeliksFeranta <gferanta@student.ciputra.ac.id>
2023-11-10 07:42:35 +0700

    Initial Commit

---
+++ W9-MobCom/W9_MobComApp.swift
@@ -0,0 +1,17 @@
+//
+//  W9_MobComApp.swift
+//  W9-MobCom
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct W9_MobComApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
